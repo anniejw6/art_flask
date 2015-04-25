b98103c5d1b594dc77fa017e58f0bad1fdd7b426
James Michelson <Jamie@Jamess-MacBook-Air.local>
2015-04-25 13:42:25 -0400

    db setupid complete

--- config.py
+++ config.py
@@ -1,24 +1,30 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config(object):
-    DEBUG = False
-    TESTING = False
-    CSRF_ENABLED = True
-    SECRET_KEY = 'this-is-a-placeholder'
-
-
-class ProductionConfig(Config):
-    DEBUG = False
+SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/appdb'
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 
-class StagingConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
+# class Config(object):
+#     DEBUG = False
+#     TESTING = False
+#     CSRF_ENABLED = True
+#     SECRET_KEY = 'this-is-a-placeholder'
 
 
-class DevelopmentConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
+# class ProductionConfig(Config):
+#     DEBUG = False
 
 
-class TestingConfig(Config):
-    TESTING = True
+# class StagingConfig(Config):
+#     DEVELOPMENT = True
+#     DEBUG = True
+
+
+# class DevelopmentConfig(Config):
+#     DEVELOPMENT = True
+#     DEBUG = True
+
+
+# class TestingConfig(Config):
+#     TESTING = True
