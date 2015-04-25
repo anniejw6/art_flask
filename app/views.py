16721210152c7677e16862f75154195eba0ec37f
Annie <anniejw6@gmail.com>
2015-04-24 22:11:33 -0400

    add rotating images

--- app/views.py
+++ app/views.py
@@ -1,6 +1,13 @@
+from flask import render_template
 from app import app
+from random import randint
 
+def randImg():
+	n = randint(1,3)
+	return "image" + str(n) + ".jpg"
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+	user = {'nickname':'Bobert'} 
+	return render_template('index.html',
+	image = randImg(), title = 'ArtFlask', user = user)
