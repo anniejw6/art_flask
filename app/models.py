db77ab448322cac9ec6c65790d51945d5ba6f27c
Annie <anniejw6@gmail.com>
2015-04-25 14:35:20 -0400

    database functional

--- app/models.py
+++ app/models.py
@@ -1,10 +1,13 @@
 from app import db
 
-# Create our database model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nickname = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
     def __repr__(self):
-        return '<User %r>' % (self.nickname)
+        return '<Name %r>' % self.name
