07764b7eda47e975d63924977dd8407d8b682885
James Michelson <Jamie@Jamess-MacBook-Air.local>
2015-04-26 15:58:31 -0400

    updated configs

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/appdb'
-#SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-#SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] 
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+if SQLALCHEMY_DATABASE_URI == None:
+	SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/appdb'
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 # class Config(object):
-#     DEBUG = False
-#     TESTING = False
-#     CSRF_ENABLED = True
-#     SECRET_KEY = 'this-is-a-placeholder'
+#     DEBUG = True
+#     # TESTING = False
+#     # CSRF_ENABLED = True
+#     SECRET_KEY = 'gogogo'
 
 
 # class ProductionConfig(Config):
