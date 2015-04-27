196a524ada90e54b8b882b888dc48c92e7bae32e
Annie <anniejw6@gmail.com>
2015-04-26 23:32:44 -0400

    update config

--- config.py
+++ config.py
@@ -5,28 +5,28 @@ SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 if SQLALCHEMY_DATABASE_URI == None:
 #	SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/appdb'
 	SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/appdb2'
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
-# class Config(object):
-#     DEBUG = True
-#     # TESTING = False
-#     # CSRF_ENABLED = True
-#     SECRET_KEY = 'gogogo'
-
-
-# class ProductionConfig(Config):
-#     DEBUG = False
+class Config(object):
+    DEBUG = True
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'gogogo'
 
 
-# class StagingConfig(Config):
-#     DEVELOPMENT = True
-#     DEBUG = True
+class ProductionConfig(Config):
+    DEBUG = False
 
 
-# class DevelopmentConfig(Config):
-#     DEVELOPMENT = True
-#     DEBUG = True
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
 
 
-# class TestingConfig(Config):
-#     TESTING = True
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
